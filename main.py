bf4918ae8ed06b7ae7e1a36d340f2d15af268835
minarashad <mina@uni.minerva.edu>
2024-11-18 19:00:51 +0800

    slice by sentences

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import numpy as np
 import requests
 import json
 import re
+import PIL
 
 print('initializing..')
 AUDIO_PATH = '/audio/'
+SLIDE_PATH = '/slides/'
 GAIN_FACTOR = 3
 # first argument is the txt file with the transcript
 transcript = sys.argv[1]
@@ -23,8 +25,7 @@ try:
 except Exception as e:
     print('Error reading file: ', e)
 
-# remove special characters with regex
-transcript_str = re.sub(r'[^a-zA-Z0-9\s]', '', transcript_str)
+
 
 # audio synthesis
 synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
@@ -49,33 +50,67 @@ def speak(text, filename):
     if not os.path.exists(current_path + AUDIO_PATH):
         os.makedirs(current_path + AUDIO_PATH)
 
-    # cut text into pieces with max length of 100 words
-    words = text.split()
+    # cut text into sentences
     text_pieces = []
-    while len(words) > 0:
-        piece = " ".join(words[:100])
-        text_pieces.append(piece)
-        words = words[100:]
+
+    # find number of sentences
+    num_sentences = len(re.findall(r'[.!?]', text))
+    while len(text) > 0:
+        # look for . or ? or ! to cut the text
+        next_dot = text.find(".")
+        next_question = text.find("?")
+        next_exclamation = text.find("!")
+        
+        endings = []
+        for end in [next_dot, next_question, next_exclamation]:
+            if end != -1:
+                endings.append(end)
+        
+        if len(endings) == 0:
+            # no more sentence endings, add the rest of the text
+            text_pieces.append(text)
+            break
+        
+        end = min(endings)+1
+
+        # get the sentence
+        sentence = text[:end].strip()
+
+        # remove the sentence from the text
+        text = text[end:]
+
+        text_pieces.append(sentence)
+
+        
+
+    
     
     for i, piece in enumerate(text_pieces):
+        
+        # print the progress
+        print(f'Processing sentence {i+1}/{num_sentences}', end='\r')
+
         speech = synthesiser(piece, forward_params={"speaker_embeddings": speaker_embedding})
         increased_volume_audio = np.clip(speech["audio"] * GAIN_FACTOR, -1.0, 1.0)
         sf.write(file_path + "_part_" + str(i) + ".wav", increased_volume_audio, samplerate=speech["sampling_rate"])
 
 def get_JSON_from_transcript(transcript):
+    print('Using OpenAI to convert the transcript to a JSON format...')
     prompt = """
 You are an experienced programmer and presenter. You have the following transcript to present. 
-make sure to change abbreviations to full words.
+make sure to change abbreviations to full words. Dont use any punctuation.
 Convert them into JSON with the following format:
 {
 slides:[
     { 
     slide: 1,
-    line: <Things to say>
+    line: <Things to say>,
+    title: <summary of things to say in 5 words at most>
     },
     {
     slide: <slide number>,
-    line: <Things to say>
+    line: <Things to say>,
+    title: <summary of things to say in 5 words at most>
     }
 ]
 }
@@ -108,73 +143,55 @@ Here is the transcript
     response = requests.post(endpoint, json=data, headers=headers)
     response_data = response.json()
     slides_JSON = response_data['choices'][0]['message']['content']
-    slides_JSON = eval(slides_JSON)
+    slides_JSON = json.loads(slides_JSON)
 
     return slides_JSON
 
-def covert_trascript_to_audio(transcript):
-    slides_JSON = get_JSON_from_transcript(transcript)
-
+def covert_trascript_to_audio(slides_JSON):
+    print('Transcript converted to JSON, starting to convert to audio...')
     for slide in slides_JSON['slides']:
         mytext = slide['line']
+        print('Converting slide ' + str(slide['slide']) + ' to audio...')
         speak(mytext, "slide" + str(slide['slide']))
 
     print("All slides have been converted to audio")
 
-print('starting')
-covert_trascript_to_audio(transcript_str)
-
-# create slide content JSON file
-def create_slide_content_json(transcript):
-    prompt = """
-
-You are an experienced programmer and presenter. You have the following transcript to present.
-Convert them into JSON with the following format:
-{
-    slides:[
-        {
-            slide: 1,
-            title: <summary of things to say in 5 words at most>
-        },
-        {
-            slide: <slide number>,
-            line: <slide title>
-        }
-    ]
-    }
-    
-    Here is the transcript:
+def generate_slides(slides_JSON):
     """
+    using the title from the JSON, we will generate an image with that title
+    black monospace text on white background
+    """
+    slide_path = os.getcwd() + SLIDE_PATH
 
-    endpoint = "https://api.openai.com/v1/chat/completions"
+    if not os.path.exists(slide_path):
+        os.makedirs(slide_path)
+    
+    print('Generating slides...')
+    for slide in slides_JSON['slides']:
+        title = slide['title']
+        slide_num = slide['slide']
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + OPENAI_API_KEY,
-    }
+        # create an image with the title
+        img = PIL.Image.new('RGB', (800, 600), color = 'white')
 
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "system",
-                "content": prompt
-            },
-            {
-                "role": "user",
-                "content": transcript
-            }
-        ]
-    }
+        fnt = PIL.ImageFont.load_default()
+        d = PIL.ImageDraw.Draw(img)
+        d.text((10,10), title, font=fnt, fill=(0,0,0))
 
-    response = requests.post(endpoint, json=data, headers=headers)
-    response_data = response.json()
-    slides_JSON = response_data['choices'][0]['message']['content']
-    slides_JSON = eval(slides_JSON)
+        img.save(slide_path+ 'slide_' + str(slide_num) + '.png')
 
-    return slides_JSON
+    print('All slides have been generated')
 
-# we need to create images with the slide summary
 
+def main():
+    print('starting')
+
+    slides_JSON = get_JSON_from_transcript(transcript_str)
+
+    covert_trascript_to_audio(slides_JSON)
+
+    generate_slides(slides_JSON)
+
+    exit(0)
 
 
